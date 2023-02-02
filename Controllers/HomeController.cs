8af97588fd49b969cb626857b4e9274428cba9f2
Ebba Hansson <ebha2100@student.miun.se>
2023-02-02 14:05:25 +0100

    Add horse functionality

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -28,12 +28,32 @@ public class HomeController : Controller
     [Route("/dinahästar")]
     public IActionResult Horses()
     {
-        // Läsa in JSON-fil
+        /* // Läsa in JSON-fil
         var jsonStr = System.IO.File.ReadAllText("horses.json");
         // Konvertera från JSON till objekt
         var jsonObj = JsonConvert.DeserializeObject<IEnumerable<Horses>>(jsonStr);
 
-        return View(jsonObj);
+        return View(jsonObj); */
+
+        return View();
+    }
+
+    [HttpPost("/dinahästar")] // POST-anrop
+    public IActionResult Horses(Horses model)
+    {
+        if(ModelState.IsValid) { // Om formulär är korrekt ifyllt
+            // Läsa in JSON-fil
+            var jsonStr = System.IO.File.ReadAllText("horses.json");
+            // Konvertera
+            var jsonObj = JsonConvert.DeserializeObject<List<Horses>>(jsonStr);
+
+            if(jsonObj != null) { // Kontrollerar att filen ej är tom
+                jsonObj.Add(model); // Lägger till häst
+                System.IO.File.WriteAllText("horses.json", JsonConvert.SerializeObject(jsonObj, Formatting.Indented)); // Formatterar om
+            }
+        }
+
+        return View();
     }
 
     [Route("/anteckningar")]
@@ -41,6 +61,7 @@ public class HomeController : Controller
     {
         return View();
     }
+
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
--- Models/Horses.cs
+++ Models/Horses.cs
@@ -1,14 +1,23 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 namespace NET_Mom2.Models {
 
     public class Horses {
 
         // Properties
         public int Id { get; set; } // ID
+
+        [Required]
         public String? HorseName { get; set; } // Namn
+
         public String? HorseNickname { get; set; } // Smeknamn
+
         public String? HorseGender { get; set; } // Kön
+
+        [Required]
         public String? HorseBreed { get; set; } // Ras
+
+        [Required]
         public int HorseLevel { get; set; } // level
 
     }
